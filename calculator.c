c6201ac067d4152d49b9244067e7804d8552c7f0
Sung-HwanLee <hwan.lee67@gmail.com>
2018-03-15 12:09:28 +0900

    Add a skeleton of calculator.c

---
+++ calculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+
+int main(){
+
+  int n, m;
+  char op;
+
+  while(1){
+    scanf("%d %c %d", &n, &op, &m);
+    switch(op) {
+    case '+' : printf("%d %c %d = %d\n", n, op, m, add(n, m)); return 0;
+    case '-' : printf("%d %c %d = %d\n", n, op, m, sub(n, m)); return 0;
+    case '*' : printf("%d %c %d = %d\n", n, op, m, mul(n, m)); return 0;
+    default : printf("invalid input\n"); break;
+    }
+  }
+
+  return 0;
+}
+
+int add(int a, int b) {
+  return 0; // TODO : FIX IT
+}
+
+int sub(int a, int b) {
+  return 0; // TODO : FIX IT
+}
+
+int mul(int a, int b) {
+  return 0; // TODO : FIX IT
+}
